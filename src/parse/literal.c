5e721604ba3bc755d4463be55bd6a8789ee6bf67
Pablo Moreira De Sousa <pmoreira@c2r7s3.42lisboa.com>
2025-05-15 16:34:48 +0100

    removing quotes setup

--- src/parse/literal.c
+++ src/parse/literal.c
@@ -6,16 +6,36 @@
 /*   By: pmoreira <pmoreira@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 11:06:07 by pmoreira          #+#    #+#             */
-/*   Updated: 2025/05/15 16:19:20 by pmoreira         ###   ########.fr       */
+/*   Updated: 2025/05/15 16:33:48 by pmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+char	*remove_quotes(char *s)
+{
+	char	*output;
+	int		count;
+	int		i;
+	t_bool	quote;
+	t_bool	d_quote;
+
+	count = 0;
+	i = -1;
+	init_proc(NULL, NULL, &quote, &d_quote);
+	while (s[++i])
+	{
+		check_quotes(s[i], &quote, &d_quote);
+		
+	}
+	free(s);
+	return (output);
+}
+
 void	find_non_expand(char *input, char **start, char **end)
 {
-	t_bool quote;
-	t_bool d_quote;
+	t_bool	quote;
+	t_bool	d_quote;
 
 	*start = NULL;
 	*end = NULL;
