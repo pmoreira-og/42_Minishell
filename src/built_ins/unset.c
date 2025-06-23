53ce67e6a316af53748ed13fcccc4777a1f8039f
pmoreira <pmoreira@student.42lisboa.com>
2025-06-23 12:38:06 +0100

    unset fixed

--- src/built_ins/unset.c
+++ src/built_ins/unset.c
@@ -6,25 +6,24 @@
 /*   By: pmoreira <pmoreira@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/13 14:10:32 by ernda-si          #+#    #+#             */
-/*   Updated: 2025/06/17 11:53:18 by pmoreira         ###   ########.fr       */
+/*   Updated: 2025/06/23 12:34:47 by pmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	unset_export(t_export **export, t_cmd *cmd, t_hell *hell)
+static void	unset_export(t_export **export, char *target)
 {
 	t_export	*temp;
 	t_export	*prev;
 
 	temp = *export;
 	prev = NULL;
-	(void)hell;
-	if (!cmd->args[1])
+	if (!target)
 		return ;
 	while (temp)
 	{
-		if (!ft_strcmp(temp->var, cmd->args[1]))
+		if (!ft_strcmp(temp->var, target))
 		{
 			if (!prev)
 				*export = temp->next;
@@ -41,19 +40,18 @@ static void	unset_export(t_export **export, t_cmd *cmd, t_hell *hell)
 	}
 }
 
-static void	unset_env(t_env **env, t_cmd *cmd, t_hell *hell)
+static void	unset_env(t_env **env, char *target)
 {
 	t_env	*temp;
 	t_env	*prev;
 
 	temp = *env;
 	prev = NULL;
-	(void)hell;
-	if (!cmd->args[1])
+	if (!target)
 		return ;
 	while (temp)
 	{
-		if (!ft_strcmp(temp->var, cmd->args[1]))
+		if (!ft_strcmp(temp->var, target))
 		{
 			if (!prev)
 				*env = temp->next;
@@ -72,7 +70,17 @@ static void	unset_env(t_env **env, t_cmd *cmd, t_hell *hell)
 
 void	mini_unset(t_env **env, t_cmd *cmd, t_export **export, t_hell *hell)
 {
-	unset_env(env, cmd, hell);
-	unset_export(export, cmd, hell);
-	update_env(hell);
+	int	i;
+	int	flag;
+
+	i = 0;
+	flag = 0;
+	while (cmd->args[++i])
+	{
+		flag = 1;
+		unset_env(env, cmd->args[i]);
+		unset_export(export, cmd->args[i]);
+	}
+	if (flag)
+		update_env(hell);
 }
